cf77bd57979c5c207681a3829de22e84afee2893
Miao ZhiCheng <miao@superfluid.finance>
2022-05-28 15:40:01 +0300

    added a typeable issue

---
+++ 2022-05-28-ghc2021-and-typeable/ghc2021-and-typeable.hs
@@ -0,0 +1,43 @@
+-- GHC version: 9.2.2
+{-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE GHC2021             #-}
+-- {-# LANGUAGE Haskell2010         #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE TypeFamilies        #-}
+
+import           Data.Kind     (Type)
+import           Data.Proxy    (Proxy (..))
+import           Data.Typeable (Typeable)
+
+
+class Typeable a => Typeable' a where
+  tag :: Proxy a -> String
+
+class Typeable' a => Typeable'' a where
+  type T1 a :: Type
+  type T2 a :: Type
+
+data A a = A
+  { val1 :: T1 a
+  , val2 :: T2 a
+  }
+
+-- * Does not work in GHC2021, but Haskell2010
+-- instance Typeable'' a => Typeable' (A a) where
+-- * Works in GHC2021
+instance (Typeable'' a, Typeable (A a)) => Typeable' (A a) where
+   tag _ = "A/" ++ tag (Proxy @a)
+
+data B
+instance Typeable' B where
+  tag _ = "B"
+instance Typeable'' B where
+  type T1 B = String
+  type T2 B = Int
+
+main :: IO ()
+main = do
+  let a = A "hello" 42 :: A B
+  putStrLn $ tag (Proxy @(A B)) ++ ": " ++ show(val1 a) ++ show(val2 a)
