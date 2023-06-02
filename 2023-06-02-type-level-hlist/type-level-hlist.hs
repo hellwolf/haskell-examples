52dce89d2f3d0293523d641a31e9ce0b0b5c4b15
Miao, ZhiCheng <zhicheng.miao@gmail.com>
2023-06-02 21:18:56 +0300

    type-level-hlist: support list type

--- 2023-06-02-type-level-hlist/type-level-hlist.hs
+++ 2023-06-02-type-level-hlist/type-level-hlist.hs
@@ -1,5 +1,6 @@
 -- -*- fill-column: 100; -*-
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TypeFamilies        #-}
 
 import           Data.Kind   (Type)
 import           Data.Monoid (Last (..))
@@ -9,7 +10,7 @@ import           Data.Proxy  (Proxy (..))
 -- Simple String Decoder Framework Using Type Level Heterogeneous List (HList)
 ----------------------------------------------------------------------------------------------------
 
-data Val a = Val a deriving Show
+data Val a = Val a
 
 type IntVal = Val Int
 type StringVal = Val String
@@ -43,12 +44,9 @@ instance forall a as. (TList a as) => TList (a :> as) () where
 instance TList () () where
   abi_decode s = (s, Nothing)
 
--- | Main API for the decoder through type variable ~t~ and its equality to the hlist form.
---
---   * Example usage: ~abiDecode @() s~
-abiDecode :: forall t a as. (TList a as, t ~ (a :> as))
-          => String -> (String, Maybe (a :> as))
-abiDecode = abi_decode @a @as
+-- abiDecode :: forall t a as. (TList a as, t ~ (a :> as))
+--           => String -> (String, Maybe (a :> as))
+-- abiDecode = abi_decode @a @as
 
 ----------------------------------------------------------------------------------------------------
 -- Supported Primitive Types
@@ -66,37 +64,49 @@ instance TList StringVal () where
 
 instance TList a () => TList [a] () where
   abi_decode ('[':s) = case go s of
-                         (Last (Just s'), Last Nothing, Just xs) -> (s', Just (xs :> ()))
-                         (Last (Just s'), _, _)                  -> (s', Nothing)
+                         (Last (Just s'), Right Nothing, xs) -> (s', Just (xs :> ()))
+                         (Last (Just s'), Left (), _)        -> (s', Nothing)
     where go s = case abi_decode @a @() s of
-            (',':s', Just (x :> ())) -> ((Last (Just s'), Last Nothing, Just [x]) <> go s')
-            (']':s', Just (x :> ())) -> ((Last (Just s'), Last Nothing, Just [x]))
-            (s', Nothing)            -> (Last (Just s'), Last (Just ()), Nothing)
+            (',':s', Just (x :> ())) -> ((Last (Just s'), Right Nothing, [x]) <> go s')
+            (']':s', Just (x :> ())) -> ((Last (Just s'), Right Nothing, [x]))
+            (s', Nothing)            -> (Last (Just s'), Left (), [])
   abi_decode s = (s, Nothing)
 
-deriving instance (Show a, Show b) => Show (a :> b)
+instance Show a => Show (Val a) where
+  show (Val a) = show a
+
+instance {-# OVERLAPPABLE #-} (Show a, Show b) => Show (a :> b) where
+  show (a :> b) = show a <> "," <> show b
+
+instance {-# OVERLAPPING  #-} (Show a, Show b) => Show (Val a :> b) where
+  show (a :> b) = "(" <> show a <> "," <> show b <> ")"
+
+instance {-# OVERLAPPING  #-} Show a => Show (Val a :> ()) where
+  show (a :> b) = show a
 
 ----------------------------------------------------------------------------------------------------
 -- Syntactic Sugar Through Establishing Equivalence Between Type Level HList & Tuples
 ----------------------------------------------------------------------------------------------------
 
-type family TUPLE_TO_TLIST a :: Type where
-  TUPLE_TO_TLIST (OneVal a) = TUPLE_TO_TLIST a :> () -- FIXME is there way to avoid such workaround?
-  TUPLE_TO_TLIST (Val a) = Val a
-  TUPLE_TO_TLIST [a] = [TUPLE_TO_TLIST a]
-  TUPLE_TO_TLIST (a, b) = TUPLE_TO_TLIST a :> TUPLE_TO_TLIST b :> ()
-  TUPLE_TO_TLIST (a, b, c) = TUPLE_TO_TLIST a :> TUPLE_TO_TLIST b :> TUPLE_TO_TLIST c :> ()
+type family EQUIV_TO_TLIST a :: Type where
+  EQUIV_TO_TLIST () = ()
+  EQUIV_TO_TLIST (a :> as) = EQUIV_TO_TLIST a :> as
+  EQUIV_TO_TLIST (Val a) = Val a
+  EQUIV_TO_TLIST [a] = [EQUIV_TO_TLIST a]
+  EQUIV_TO_TLIST (a, b) = EQUIV_TO_TLIST a :> EQUIV_TO_TLIST b :> ()
+  EQUIV_TO_TLIST (a, b, c) = EQUIV_TO_TLIST a :> EQUIV_TO_TLIST b :> EQUIV_TO_TLIST c :> ()
 
-abiDecode' :: forall t a as. (TList a as, TUPLE_TO_TLIST t ~ (a :> as))
-           => Proxy t -> String -> (String, Maybe (a :> as))
-abiDecode' _ = abi_decode @a @as
+-- | Main API for the decoder through type variable ~t~ and its equivalent hlist form.
+abiDecode :: forall t a as. (TList a as, EQUIV_TO_TLIST t ~ (a :> as))
+           => String -> (String, Maybe (a :> as))
+abiDecode = abi_decode @a @as
 
 ----------------------------------------------------------------------------------------------------
 -- Test Code
 ----------------------------------------------------------------------------------------------------
 main :: IO ()
 main = do
-  -- Use "Sugarfree" HList Syntax
+  -- Use "sugarfree" hlist syntax
   print $ abiDecode @TNil ""
   print $ abiDecode @(IntVal :> ())
     "42"
@@ -108,16 +118,16 @@ main = do
     "42,(\"estonia\",14),\"finland\""
   print $ abiDecode @(IntVal :> (StringVal :> IntVal :> ()) :> (StringVal :> IntVal :> ()) :> ())
     "42,(\"estonia\",14),(\"finland\",50)"
-  -- Use Tuple Syntax
-  print $ abiDecode' (Proxy @(OneVal IntVal))
+  -- Use (nicer) tuple syntax
+  print $ abiDecode @(OneVal IntVal) -- FIXME is there way to avoid such workaround?
     "42"
-  print $ abiDecode' (Proxy @(IntVal, (StringVal, IntVal)))
+  print $ abiDecode @(IntVal, (StringVal, IntVal))
     "42,(\"france\",41)"
-  print $ abiDecode' (Proxy @(StringVal, [IntVal]))
+  print $ abiDecode @(StringVal, [IntVal])
     "\"germany\",[1,3,5,7,11]"
-  print $ abiDecode' (Proxy @(StringVal, [(IntVal, IntVal)]))
+  print $ abiDecode @(StringVal, [(IntVal, IntVal)])
     "\"germany\",[(1,3),(5,7),(11,13)]"
-  print $ abiDecode' (Proxy @(StringVal, [[IntVal]]))
+  print $ abiDecode @(StringVal, [[IntVal]])
     "\"germany\",[[1],[3,5,7]]"
   -- Use patter matching to consume the parsed values
   case abiDecode @(IntVal :> IntVal :> ()) "4,2" of
